416912aebaa8d7872788687fe0f84f4310247ddc
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-07-22 00:33:57 -0700

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState } from "react";
+import Todoform from "./components/todoform/Todoform";
+import Todo from "./components/todo/Todo";
 
 function App() {
+  let [todos ,setTodos] = useState([]); // why let /be can you change for (if) fx;
+  const addTodo =(todo)=>{
+    setTodos([todo, ...todos]);
+};
+const handleDelete = (id)=>{
+setTodos(todos.filter((todo)=> todo.id !== id))
+};
+//three btn
+const [todoToShow,setTodoToShow] = useState("all");
+ const updateTodoToShow =(s)=>{
+  setTodoToShow(s)
+ };
+
+//  finshtask
+ const toggleComplete = (id)=> {
+
+    setTodos(
+      //loop
+      todos.map((todo) => {
+        //if
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete,
+          };
+
+        } else {
+          return todo; // same
+
+        }
+      })
+    );
+
+
+
+
+  };
+
+
+
+
+
+
+//  if condation for three btn
+if (todoToShow === "active") {
+  todos =todos.filter((todo)=>!todo.complete);   //filter becose target a tasks  uncomblte===true becose value comblte =false 
+}else if(todoToShow === "complete"){
+  todos =todos.filter((todo)=> todo.complete) //just comblete
+     
+
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <Todoform onSubmit={addTodo}/>
+
+         {todos.map((todo) => (
+           <Todo
+            todo={todo} 
+            key={todo.id}
+            onDelete={() =>  handleDelete(todo.id)}
+            toggleComplete ={()=> toggleComplete(todo.id)}
+            
+            />
+))}
+    <div>
+        <button className='update-btn btn'onClick={()=> updateTodoToShow("all")}>all</button>
+        <button className='update-btn btn'onClick={()=> updateTodoToShow("active")}>active</button>
+        <button className='update-btn btn'onClick={()=> updateTodoToShow("complete")}>complete</button>
+     </div>
+     <button>Remove all comblete todos</button>
+     <button>toggle All Complete</button>
+
+
+
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/todoform/Todoform.jsx
@@ -0,0 +1,38 @@
+
+import {React, useState,} from 'react'
+import './Todoform.css'
+import shortid from 'shortid';
+
+
+
+const Todoform = (props) => {
+    const [text ,setText]= useState("");
+
+    const handleSubmit = (e)=>{//when clikd on btn submit the task
+        e.preventDefault(); 
+        props.onSubmit({ //from app.js
+            id:shortid.generate(), //id form task throgh npm i shortid
+            text: text, //task name
+            complete :false ,//bollan value deffult
+        });
+         setText("")
+    };
+
+   const handleChange = (e) =>{
+        setText(e.target.value)
+    };
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" className='input-field' onChange={handleChange} value={ text} />
+            <button className='btn' onClick={handleSubmit}>add Todo</button>
+        </form>
+
+
+
+
+    </div>
+  );
+};
+
+export default Todoform
